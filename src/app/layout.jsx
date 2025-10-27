bd6c1d156552790ca9333442b048f25841f3a067
Jecheon Ryu <rjc1704@gmail.com>
2025-10-27 15:34:43 +0900

    스타일링 실습준비

---
+++ src/app/layout.jsx
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "고양이 정보 사이트",
+  description: "다양한 고양이 품종에 대한 정보를 제공하는 웹사이트",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
